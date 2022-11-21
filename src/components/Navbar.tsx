563e17cfd02be8c97de339ca848b1ef3c4fe9c74
jonathan <jonathan@jeudi8.io>
2022-11-21 22:23:00 +0100

    feat: add default map  background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 const Navbar = () => {
   const navigate = () => (window.location.href = '/');
   return (
-    <AppBar color="transparent" role="navigation">
+    <AppBar color="transparent" role="navigation" sx={{ boxShadow: 'none' }}>
       <Toolbar>
         <Button aria-label="Navigate to homepage" onClick={navigate}>
           <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
