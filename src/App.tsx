872b900f7fd3763465e3e3f702d1c6d6646e1fbf
jonathan <jonathan@jeudi8.io>
2022-11-21 19:12:22 +0100

    build: init mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { Card, CardContent } from '@mui/material';
+import { Forward } from '@mui/icons-material';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Card>
+        <CardContent>
+          <a href="https://vitejs.dev" target="_blank">
+            <Forward />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </CardContent>
+      </Card>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
