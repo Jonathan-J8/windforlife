31a0d0d6611a187bb93c32136605b6b95dc64505
jonathan <jonathan@jeudi8.io>
2022-11-22 20:23:54 +0100

    feat: MarkerDetail

--- src/components/MarkerDetail.tsx
+++ src/components/MarkerDetail.tsx
@@ -1,22 +1,46 @@
-import { Box, Grow, Modal, Paper } from '@mui/material';
-import { useAnemometerState } from '../anemometer/store';
+import { Close } from '@mui/icons-material';
+import { Box, Button, Divider, Fab, Grow, Modal, Paper, Typography } from '@mui/material';
+import { useAnemometerAction, useAnemometerState, AnemometerAction } from '../anemometer/store';
 
 const MarkerDetail = () => {
   const anemometer = useAnemometerState();
+  const dispatch = useAnemometerAction();
 
+  const onClose = () => dispatch({ type: AnemometerAction.REMOVE });
   const show = anemometer ? true : false;
+  const name = anemometer?.name;
+  const lat = anemometer?.loc?.lat;
+  const long = anemometer?.loc?.long;
+  const weeklyForce = anemometer?.statistics?.average?.weekly?.force || '';
+  const dailyForce = anemometer?.statistics?.average?.daily?.force || '';
+
   return (
-    <Grow in={show}>
+    <Grow in={show} style={{ transformOrigin: '0 0 0' }}>
       <Paper
         elevation={2}
-        style={{
+        sx={{
+          p: 2,
           zIndex: 1000,
           position: 'absolute',
           top: '100px',
           left: '15px',
           minWidth: '300px',
           minHeight: '300px',
-        }}></Paper>
+        }}>
+        <Button onClick={onClose}>
+          <Close></Close>
+        </Button>
+        <Typography variant="body1" component="h2">
+          {name}
+        </Typography>
+        <Divider />
+        <Typography variant="body2">Latitude {lat}</Typography>
+        <Typography variant="body2">Longitude {long}</Typography>
+        <Divider />
+        <Typography variant="body2">Weekly force average {weeklyForce}</Typography>
+        <Typography variant="body2">Daily force average {dailyForce}</Typography>
+        <Divider />
+      </Paper>
     </Grow>
 
     // <Modal open={open} onClose={onClose} aria-labelledby="title" aria-describedby="description">
