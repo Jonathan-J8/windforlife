16efc945419ce38dd388a07b0557163927a23413
jonathan <jonathan@jeudi8.io>
2022-11-22 15:54:42 +0100

    perf: prevent Map rerender

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from 'react';
+import { memo, PropsWithChildren } from 'react';
 import type { LeafletMouseEvent, Map as LeafletMap } from 'leaflet';
 import { MapContainer, TileLayer, type MapContainerProps } from 'react-leaflet';
 
@@ -6,36 +6,44 @@ import useRefWithCallback from '../utils/useRefWithCallback';
 
 interface MapProps extends MapContainerProps, PropsWithChildren {}
 
-const MapCustom = ({ center, zoom, children }: MapProps) => {
-  const setRef = useRefWithCallback((map: LeafletMap | undefined) => {
-    const onMove = (e: LeafletMouseEvent) => {
-      const map = e.target;
-      console.log(map.getCenter());
-    };
+const Map = memo(
+  ({
+    children,
+    center = [21.022982546427425, -158.15368652343753],
+    zoom = 7,
+    scrollWheelZoom = true,
+    ...rest
+  }: MapProps) => {
+    const setRef = useRefWithCallback((map: LeafletMap | undefined) => {
+      const onMove = (e: LeafletMouseEvent) => {
+        const map = e.target;
+        console.log(map.getCenter());
+      };
 
-    if (map) map.on('click', onMove);
+      if (map) map.on('click', onMove);
 
-    return () => {
-      if (map) map.off('click', onMove);
-      map = undefined;
-    };
-  });
+      return () => {
+        if (map) map.off('click', onMove);
+      };
+    });
+    console.log('> rednerder');
 
-  return (
-    <MapContainer ref={setRef} center={center} zoom={zoom} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {children}
-      {/* <Marker position={[21.663522, -158.052865]}>
+    return (
+      <MapContainer ref={setRef} {...rest} center={center} zoom={zoom} scrollWheelZoom>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {children}
+        {/* <Marker position={[21.663522, -158.052865]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
           
         </Marker> */}
-    </MapContainer>
-  );
-};
+      </MapContainer>
+    );
+  }
+);
 
-export default MapCustom;
+export default Map;
--- src/utils/useRefWithCallback.ts
+++ src/utils/useRefWithCallback.ts
@@ -4,18 +4,23 @@ type Unmount = () => void;
 type Mount = (node: any) => Unmount;
 
 const useRefWithCallback = (onMount: Mount) => {
-  const nodeRef = useRef<any>(null);
+  const ref = useRef<any>(undefined);
   const callback = useRef<Unmount | undefined>(undefined);
 
   const setRef = useCallback(
     (node: any) => {
+      // execute the unmount method if any
       if (typeof callback.current === 'function') callback.current();
 
+      // flush previous values
       callback.current = undefined;
-      nodeRef.current = node;
+      if (ref.current) ref.current = undefined;
 
-      if (nodeRef.current) callback.current = onMount(nodeRef.current);
+      // start with fresh values
+      ref.current = node;
+      if (ref.current) callback.current = onMount(ref.current);
     },
+    // reset each time the component mount
     [onMount]
   );
 
