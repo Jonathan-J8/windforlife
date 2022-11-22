4b145e9ddd3b7b4d1bdb79133e85ad4382f499c8
jonathan <jonathan@jeudi8.io>
2022-11-22 22:50:52 +0100

    refactor: map center point

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,31 @@
 import { memo, type PropsWithChildren } from 'react';
 import { MapContainer, TileLayer, type MapContainerProps } from 'react-leaflet';
+import type { LeafletMouseEvent, Map as LeafletMap } from 'leaflet';
+import useRefWithCallback from '../utils/useRefWithCallback';
 
 interface MapProps extends MapContainerProps, PropsWithChildren {}
 
 const Map = memo(
   ({
     children,
-    center = [21.022982546427425, -158.15368652343753],
+    center = [20.511303413289298, -156.70894456095994],
     zoom = 7,
     scrollWheelZoom = true,
     zoomControl = false,
     ...rest
   }: MapProps) => {
+    const setRef = useRefWithCallback((map: LeafletMap | undefined) => {
+      const onCLick = (e: LeafletMouseEvent) => {
+        const map = e.target;
+        console.log(map.getCenter());
+      };
+      if (map) map.on('click', onCLick);
+      return () => {
+        if (map) map.off('click', onCLick);
+      };
+    });
     return (
-      <MapContainer center={center} zoom={zoom} scrollWheelZoom zoomControl={zoomControl} {...rest}>
+      <MapContainer ref={setRef} center={center} zoom={zoom} scrollWheelZoom zoomControl={zoomControl} {...rest}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
